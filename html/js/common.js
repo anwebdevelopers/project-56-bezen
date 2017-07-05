329323491fd3cdc5041d02d9845aa34aecce844b
anwebdevelopers <anwebdev78@gmail.com>
2017-07-05 20:06:10 +0300

    05.07.2017

--- html/js/common.js
+++ html/js/common.js
@@ -3,6 +3,43 @@ $(function() {
     'use strict';
 
     /*******************************************************/
+    //VARS
+    /*******************************************************/
+    var $window = $(window),
+        $html = $('html'),
+        w = $window.width();
+
+
+    /*******************************************************/
+    //MENU MOBILE
+    /*******************************************************/
+    var $headerMenu = $('.header__menu'),
+        $headerButtonMenu = $('.header__button-menu');
+    $headerButtonMenu.click(function() {
+        $(this).toggleClass('active');
+        $headerMenu.toggleClass('active');
+    });
+
+    /*******************************************************/
+    //SUBMENU MOBILE
+    /*******************************************************/
+    $headerMenu.find('a').click(function(e) {
+        if (w <= 960) {
+            var $this = $(this),
+                $ul = $this.next('ul'),
+                $li = $this.closest('li');
+            if ($ul.length && !$li.hasClass('active')) {
+                e.preventDefault();
+                $ul.slideDown(300);
+                $li.addClass('active').siblings().removeClass('active').find('ul').slideUp(300);
+            } else {
+                $headerMenu.toggleClass('active');
+                $headerButtonMenu.toggleClass('active');
+            }
+        }
+    });
+
+    /*******************************************************/
     //First Screen Slider
     /*******************************************************/
     var $headerBanner = $('.header__banner');
@@ -15,18 +52,51 @@ $(function() {
             items: 1,
             nav: true,
             navText: '',
-            autoplayTimeout: 10000,
             autoplay: true,
+            autoplayTimeout: 10000,
             smartSpeed: 1200
         });
     }
+
+    //------------------------------------------------------------
+    //Slider gallery
+    //------------------------------------------------------------
+    $('.news__box').addClass('owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        navText: '',
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 600,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            481: {
+                items: 2
+            },
+            961: {
+                items: 3
+            }
+        }
+    });
+
+    //------------------------------------------------------------
+    //Hover fo IOS
+    //------------------------------------------------------------
+    $('.news__item').hover(function() {
+        $(this).addClass('active');
+    }, function() {
+        $(this).removeClass('active');
+    });
 
     /*******************************************************/
     //Chrome Smooth Scroll
     /*******************************************************/
     try {
         $.browserSelector();
-        if ($('html').hasClass('chrome')) {
+        if ($html.hasClass('chrome')) {
             $.smoothScroll();
         }
     } catch (err) {
@@ -43,9 +113,10 @@ $(function() {
 
     var $header = $('.header');
     function fullscreen() {
-        if($('html').hasClass('ie') || $('html').hasClass('gecko')) {
-            $header.removeAttr('style');
-            var windowHeight = $(window).height(),
+        $header.removeAttr('style');
+        if($html.hasClass('ie') || $html.hasClass('gecko')) {
+
+            var windowHeight = $window.height(),
                 headerHeight = $header.height();
             if ( windowHeight >= headerHeight ) {
                 $header.css({
@@ -55,7 +126,20 @@ $(function() {
         }
     }
     fullscreen();
-    $(window).resize(function() {
+
+
+    $window.resize(function() {
+
+        $headerMenu.find('ul, li').removeAttr('style').removeClass('active');
+
         fullscreen();
+    });
+
+    /*******************************************************/
+    //SECTION COUNTER
+    /*******************************************************/
+    var $section = $('.section');
+    $section.each(function() {
+        $(this).find('[class*="__container"]').prepend('<div class="counter"><span class="counter__number">' + ($(this).index('.section') + 1) + '</span><span class="counter__quantity"> / ' + $section.length + '</span></div>');
     });
 });
